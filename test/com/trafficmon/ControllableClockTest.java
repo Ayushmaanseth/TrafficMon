93956c772675781dde4c84a72fd086928712b6af
lovepreetsi <32802848+lovepreetsi@users.noreply.github.com>
2018-12-08 19:50:48 +0000

    added tests for custom clock

--- test/com/trafficmon/ControllableClockTest.java
+++ test/com/trafficmon/ControllableClockTest.java
@@ -1,23 +1,89 @@
 package com.trafficmon;
-
-import static junit.framework.TestCase.assertEquals;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.CoreMatchers.containsString;
-
-import org.jmock.Expectations;
-import org.jmock.integration.junit4.JUnitRuleMockery;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.math.BigDecimal;
+import java.time.temporal.ChronoUnit.*;
+
+import static java.time.temporal.ChronoUnit.MINUTES;
+import static java.time.temporal.ChronoUnit.SECONDS;
+import static junit.framework.TestCase.*;
+
+import java.time.LocalTime;
+
 
 public class ControllableClockTest {
 
-    private final  Clock clock = new ControllableClock();
+
+    @Test
+    public void IfClockTimesSame() {
+          ControllableClock clock = new ControllableClock();
+        clock.currentTimeIs(10, 00);
+         ControllableClock clock2 = new ControllableClock();
+        clock2.currentTimeIs(10, 00);
+
+        assertTrue(clock.now().equals(clock2.now()));
+
+    }
+
+
+    @Test
+    public void IfdifferentClockTime() {
+        ControllableClock clock = new ControllableClock();
+        clock.currentTimeIs(10, 00);
+        ControllableClock clock2 = new ControllableClock();
+        clock2.currentTimeIs(10, 01);
+
+        assertFalse(clock.now().equals(clock2.now()));
+    }
+
+    @Test
+    public void comparisonbetweenClockTime() {
+        ControllableClock clock = new ControllableClock();
+        clock.currentTimeIs(10, 00);
+        ControllableClock clock2 = new ControllableClock();
+        clock2.currentTimeIs(10, 01);
+
+        assertTrue(clock.now().compareTo(clock2.now()) < 0);
+
+    }
+
+    @Test
+    public void MindifferenceBetweenClockTime() {
+        ControllableClock clock = new ControllableClock();
+        clock.currentTimeIs(10, 00);
+        ControllableClock clock2 = new ControllableClock();
+        clock2.currentTimeIs(10, 11);
+
+        assertEquals(clock.now().until(clock2.now(),MINUTES),11);
+
+    }
+
+    @Test
+    public void gethourClockTime() {
+        ControllableClock clock = new ControllableClock();
+        clock.currentTimeIs(10, 00);
+        ControllableClock clock2 = new ControllableClock();
+        clock2.currentTimeIs(10, 11);
+
+
+
+        assertEquals(clock.now().getHour(),10);
+
+    }
+
+    @Test
+    public void getMinuteClockTime() {
+        ControllableClock clock = new ControllableClock();
+        clock.currentTimeIs(10, 00);
+        ControllableClock clock2 = new ControllableClock();
+        clock2.currentTimeIs(10, 11);
+
+
+
+        assertEquals(clock.now().getMinute(), 00);
+
+    }
+
 
 }
+
+
